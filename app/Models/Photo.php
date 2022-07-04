5904638e7a0066e8e903b24b0f005e64821c0b67
agahebeye <agahebeye@gmail.com>
2022-07-04 12:12:07 +0200

    wip

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Photo extends Model
 {
     use HasFactory;
+    protected $fillabe = ['src'];
+
+    public function photoable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
--- database/migrations/2022_07_04_100433_create_photos_table.php
+++ database/migrations/2022_07_04_100433_create_photos_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('src');
+            $table->morphs('photoable');
         });
     }
 
