5ea72784a5343dea50111ca6ee6f732a72539877
agahebeye <agahebeye@gmail.com>
2022-07-04 12:17:31 +0200

    wip

--- app/Http/Controllers/UserPhotoController.php
+++ app/Http/Controllers/UserPhotoController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserPhotoController extends Controller
 {
-    //
+    public function create(): \Inertia\Response
+    {
+        return Inertia::render('User/Photo/Create.vue');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserPhotoController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -28,4 +18,7 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('{user}/photos/create', [UserPhotoController::class, 'create'])->name('users.photos.create');
+Route::post('{user}/photos/store', [UserPhotoController::class, 'store'])->name('users.photos.store');
+
+require __DIR__ . '/auth.php';
