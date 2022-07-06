36a5e88790adafc933cf469726e41b21568abe1a
agahebeye <agahebeye@gmail.com>
2022-07-06 13:03:28 +0200

    wip

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -10,11 +10,11 @@ class DocumentController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function index()
     {
-        //
+        return Inertia::render('Documents/Index');
     }
 
     /**
