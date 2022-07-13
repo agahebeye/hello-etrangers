122fd4b33191149f45935af99348b3135f375755
agahebeye <agahebeye@gmail.com>
2022-07-13 14:26:15 +0200

    wip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import ImageSlider from '@/Components/ImageSlider.vue';
 </script>
 
 <template>
@@ -17,11 +18,7 @@ import { Head } from '@inertiajs/inertia-vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        Accueil
-                    </div>
-                </div>
+                <ImageSlider />
             </div>
         </div>
     </BreezeAuthenticatedLayout>
