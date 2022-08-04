eff3c1416d60852fa8cb2a528f59c344196f0979
agahebeye <agahebeye@gmail.com>
2022-08-04 08:44:57 +0200

    add factory to Faculty

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Faculty extends Model
 {
     use HasFactory;
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /** @var array<int, string> */
+    protected $fillable = ['name'];
+
+    public function universities(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(University::class, table: 'universities_faculties');
+    }
 }
