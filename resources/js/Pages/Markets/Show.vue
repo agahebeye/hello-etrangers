02b393d0e50d2f7aeed3d1d31f4b9fa23939536c
agahebeye <agahebeye@gmail.com>
2022-08-05 16:49:03 +0200

    wip

--- resources/js/Pages/Markets/Show.vue
+++ resources/js/Pages/Markets/Show.vue
@@ -1,9 +1,30 @@
 <script setup>
-defineProps({
-    market: Object
-});
-</script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 
+defineProps({
+    market: Array,
+})
+</script>
 <template>
-    <div>{{ market }}</div>
+
+    <Head :title="`Marchés/${market.name}`" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Marchés / <span class="text-gray-500">{{ market.name }}</span>
+            </h2>
+        </template>
+
+
+        <div class="prose max-w-none">
+            <div class="mb-6 featured_image">
+                <div class="w-full leading-[18rem] text-center bg-white h-72">Photo ici...</div>
+            </div>
+
+            <h1> {{ market.name }}</h1>
+        </div>
+        
+    </BreezeAuthenticatedLayout>
 </template>
--- resources/js/Pages/Universities/Show.vue
+++ resources/js/Pages/Universities/Show.vue
@@ -1,9 +1,30 @@
 <script setup>
-defineProps({
-    university: Object
-});
-</script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 
+defineProps({
+    university: Array,
+})
+</script>
 <template>
-    {{ university }}
+
+    <Head :title="`Universités/${university.name}`" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Universités / <span class="text-gray-500">{{ university.name }}</span>
+            </h2>
+        </template>
+
+
+        <div class="prose max-w-none">
+            <div class="mb-6 featured_image">
+                <div class="w-full leading-[18rem] text-center bg-white h-72">Photo ici...</div>
+            </div>
+
+            <h1> {{ university.name }}</h1>
+        </div>
+        
+    </BreezeAuthenticatedLayout>
 </template>
