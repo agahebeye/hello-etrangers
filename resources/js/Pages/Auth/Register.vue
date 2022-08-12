4f447733b6a465df2ca5a9cf681518676d05902b
agahebeye <agahebeye@gmail.com>
2022-08-12 06:38:55 +0200

    refactor Register.vue

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -33,45 +33,45 @@ const submit = () => {
         <form @submit.prevent="submit">
             <div>
                 <BreezeLabel for="lastname" value="Nom" />
-                <BreezeInput id="lastname" type="text" class="mt-1 block w-full" v-model="form.lastname" required autofocus autocomplete="name" />
+                <BreezeInput id="lastname" type="text" class="block w-full mt-1" v-model="form.lastname" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="firstname" value="Prénom" />
-                <BreezeInput id="firstname" type="text" class="mt-1 block w-full" v-model="form.firstname" required autofocus autocomplete="name" />
+                <BreezeInput id="firstname" type="text" class="block w-full mt-1" v-model="form.firstname" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="email" value="Email" />
-                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="username" />
+                <BreezeInput id="email" type="email" class="block w-full mt-1" v-model="form.email" required autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="password" value="Mot de passe" />
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+                <BreezeInput id="password" type="password" class="block w-full mt-1" v-model="form.password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="password_confirmation" value="Confirmer mot de passe" />
-                <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+                <BreezeInput id="password_confirmation" type="password" class="block w-full mt-1" v-model="form.password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
                 <BreezeLabel for="role" value="Role" />
-                <div class="flex space-x-3 my-3">
+                <div class="flex my-3 space-x-3">
                     <div class="flex space-x-2">
-                        <input type="radio" for="role" id="commercant" value="Commercant" v-model="form.role" required />
-                        <BreezeLabel value="Commerçant" />
+                        <input type="radio" for="role" id="commercant" value="Commercant" v-model="form.role" />
+                        <BreezeLabel value="Commerçant" id="role" />
                     </div>
                     <div class="flex space-x-2">
-                        <input type="radio" id="etudiant" value="Etudiant" v-model="form.role" required />
-                        <BreezeLabel for="role" value="Etudiant"></BreezeLabel>
+                        <input type="radio" for="role" id="etudiant" value="Etudiant" v-model="form.role" />
+                        <BreezeLabel id="role" value="Etudiant"></BreezeLabel>
                     </div>
                 </div>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                <Link :href="route('login')" class="text-sm text-gray-600 underline hover:text-gray-900">
                 Déjà enregistré(e)?
                 </Link>
 
