8d11bf64a0aa0b7d27200eab7a393d196a111635
agahebeye <agahebeye@gmail.com>
2022-08-12 10:43:32 +0200

    remove all route files

--- routes/University.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\UniversityController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('universities', [UniversityController::class, 'index']);
--- routes/User.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-Route::get('{user}/photos/create', [UserPhotoController::class, 'create'])->name('users.photos.create');
-Route::post('{user}/photos/store', [UserPhotoController::class, 'store'])->name('users.photos.store');
