50fb72aa2ef76b8360f4e07af563c86af8b448d1
agahebeye <agahebeye@gmail.com>
2022-08-16 12:51:26 +0200

    add contents to CitySeeder

--- database/migrations/2022_07_12_070204_create_cities_table.php
+++ database/migrations/2022_07_12_070204_create_cities_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->decimal('superficies');
+            $table->decimal('population');
         });
     }
 
