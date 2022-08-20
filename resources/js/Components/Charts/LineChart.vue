89d16ecc9ee3c4405f4bdff886d8822369aab09e
agahebeye <agahebeye@gmail.com>
2022-08-20 17:11:39 +0200

    WIP

--- resources/js/Components/Charts/LineChart.vue
+++ resources/js/Components/Charts/LineChart.vue
@@ -1,5 +1,34 @@
 <script setup>
 import { LineChart } from "vue-chart-3"
+import {ref, computed} from 'vue';
+
+const dataValues = ref([12, 14, 16, 18, 11, 13, 15])
+
+const data = computed(() => ({
+  labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+
+  datasets: [
+    {
+      label: "Foo",
+      data: dataValues.value,
+      borderColor: "#dc322f"
+    }
+  ]
+}))
+
+const options = ref({
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  },
+
+  plugins: {
+    title: {
+      text: "Line"
+    }
+  }
+})
 </script>
 
 <template>
--- resources/js/plugins/chart.js
+++ resources/js/plugins/chart.js
@@ -2,13 +2,15 @@ import {
     Chart, Legend, Title,
     Tooltip,
     DoughnutController, ArcElement,
-    Chart, LinearScale, LineElement,
+    LinearScale, LineElement,
     LineController, CategoryScale, PointElement
 } from "chart.js"
 
 Chart.register(
     Legend, Title, Tooltip,
     DoughnutController, ArcElement,
+    LinearScale, LineElement,
+    LineController, CategoryScale, PointElement,
 )
 
 Chart.defaults.plugins.title.display = true
