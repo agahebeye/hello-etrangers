225ea3ceb6a0024803550de50eeb542d1b4ae773
agahebeye <agahebeye@gmail.com>
2022-08-22 17:57:32 +0200

    WIP

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,12 +3,24 @@ import AuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import NavLink from '@/Components/NavLink.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 
+import { DoughnutChart,  } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
 import { ref } from 'vue'
-
-import BarChar from '@/Components/Charts/BarChar.vue';
 import ChartDoughnut from '@/Components/Charts/ChartDoughnut.vue';
-import LineChart from '@/Components/Charts/LineChart.vue';
 
+const doughnutData = ref({
+    labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+    datasets: [
+        {
+            data: [30, 40, 60, 70, 5],
+            backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+        },
+    ],
+});
+// import BarChar from '@/Components/Charts/BarChar.vue';
+// import ChartDoughnut from '@/Components/Charts/ChartDoughnut.vue';
+// import LineChart from '@/Components/Charts/LineChart.vue';
+Chart.register(...registerables);
 
 defineProps({
     latestDocuments: Array,
@@ -144,134 +156,133 @@ const foreignerHeadings = ref([{
             </div>
 
             <div id="charts" class="relative z-10 flex flex-wrap justify-between gap-10">
-                <BarChar />
-                <ChartDoughnut />
-                <LineChart />
-            </div>
-
-            <!--recent docs-->
-            <div>
-                <h2>Documents récement commandés</h2>
-                <NavLink class="block mb-4 no-underline" :href="route('documents.index')">Voir plus de documents</NavLink>
-                <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
-                    <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
-                        <thead>
-                            <tr class="text-left">
-                                <template v-for="heading in documentHeadings">
-                                    <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
-                                        v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
-                                </template>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <template v-for="document in latestDocuments">
-                                <tr>
-                                    <td class="border-t border-gray-200 border-dashed userId">
-                                        <span class="px-4 text-gray-700 ">{{ document.id }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed owner">
-                                        <span class="px-4 text-gray-700 ">{{ document.user.fullname }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed adress">
-                                        <span class="px-4 text-gray-700 ">{{ document.user.adress?.avenue ?? 'somewhere in Burundi' }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed visa_status">
-                                        <span v-if="!document.validated_at && !document.rejected_at" class="px-4 py-1 ml-4 text-xs text-yellow-600 bg-yellow-200 rounded-full">Programmé</span>
-                                        <span v-if="document.rejected_at" class="px-4 py-1 ml-4 text-xs text-red-600 bg-red-200 rounded-full">Annulé</span>
-                                        <span v-if="document.validated_at" class="px-4 py-1 ml-4 text-xs text-green-600 bg-green-200 rounded-full">Validé</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed visa_kind">
-                                        <span class="px-4 text-gray-700">{{ document.visa_kind }}</span>
-                                    </td>
-                                </tr>
-                            </template>
-                        </tbody>
-                    </table>
-                </div>
+                <ChartDoughnut :chartData="doughnutData" />
             </div>
 
             <div>
-                <h2>Etudiants récents</h2>
-                <NavLink class="block mb-4 no-underline" :href="route('foreigners.index', { role: 'Etudiant' })">Voir plus d'Etudiants</NavLink>
-                <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
-                    <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
-                        <thead>
-                            <tr class="text-left">
-                                <template v-for="heading in foreignerHeadings">
-                                    <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
-                                        v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
-                                </template>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <template v-for="foreigner in latestStudents">
-                                <tr>
-                                    <td class="border-t border-gray-200 border-dashed userId">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.id }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed first_name">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.first_name }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed lastName">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.last_name }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed emailAddress">
-                                        <span class="px-4 text-gray-700">{{ foreigner.email }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed gender">
-                                        <span class="px-4 text-gray-700">{{ foreigner.document.gender }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed phoneNumber">
-                                        <span class="px-4 text-gray-700">{{ foreigner.document.citizenship }}</span>
-                                    </td>
+                <!--recent docs-->
+                <div>
+                    <h2>Documents récement commandés</h2>
+                    <NavLink class="block mb-4 no-underline" :href="route('documents.index')">Voir plus de documents</NavLink>
+                    <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
+                        <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
+                            <thead>
+                                <tr class="text-left">
+                                    <template v-for="heading in documentHeadings">
+                                        <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
+                                            v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
+                                    </template>
                                 </tr>
-                            </template>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <template v-for="document in latestDocuments">
+                                    <tr>
+                                        <td class="border-t border-gray-200 border-dashed userId">
+                                            <span class="px-4 text-gray-700 ">{{ document.id }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed owner">
+                                            <span class="px-4 text-gray-700 ">{{ document.user.fullname }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed adress">
+                                            <span class="px-4 text-gray-700 ">{{ document.user.adress?.avenue ?? 'somewhere in Burundi' }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed visa_status">
+                                            <span v-if="!document.validated_at && !document.rejected_at" class="px-4 py-1 ml-4 text-xs text-yellow-600 bg-yellow-200 rounded-full">Programmé</span>
+                                            <span v-if="document.rejected_at" class="px-4 py-1 ml-4 text-xs text-red-600 bg-red-200 rounded-full">Annulé</span>
+                                            <span v-if="document.validated_at" class="px-4 py-1 ml-4 text-xs text-green-600 bg-green-200 rounded-full">Validé</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed visa_kind">
+                                            <span class="px-4 text-gray-700">{{ document.visa_kind }}</span>
+                                        </td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div>
+                    <h2>Etudiants récents</h2>
+                    <NavLink class="block mb-4 no-underline" :href="route('foreigners.index', { role: 'Etudiant' })">Voir plus d'Etudiants</NavLink>
+                    <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
+                        <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
+                            <thead>
+                                <tr class="text-left">
+                                    <template v-for="heading in foreignerHeadings">
+                                        <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
+                                            v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
+                                    </template>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <template v-for="foreigner in latestStudents">
+                                    <tr>
+                                        <td class="border-t border-gray-200 border-dashed userId">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.id }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed first_name">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.first_name }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed lastName">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.last_name }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed emailAddress">
+                                            <span class="px-4 text-gray-700">{{ foreigner.email }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed gender">
+                                            <span class="px-4 text-gray-700">{{ foreigner.document.gender }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed phoneNumber">
+                                            <span class="px-4 text-gray-700">{{ foreigner.document.citizenship }}</span>
+                                        </td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div>
+                    <h2>Commerçants récement ajoutés</h2>
+                    <NavLink class="block mb-4 no-underline" :href="route('foreigners.index', { role: 'Commerçant' })">Voir plus de Commerçants</NavLink>
+                    <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
+                        <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
+                            <thead>
+                                <tr class="text-left">
+                                    <template v-for="heading in foreignerHeadings">
+                                        <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
+                                            v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
+                                    </template>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <template v-for="foreigner in latestTraders">
+                                    <tr>
+                                        <td class="border-t border-gray-200 border-dashed userId">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.id }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed first_name">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.first_name }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed lastName">
+                                            <span class="px-4 text-gray-700 ">{{ foreigner.last_name }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed emailAddress">
+                                            <span class="px-4 text-gray-700">{{ foreigner.email }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed gender">
+                                            <span class="px-4 text-gray-700">{{ foreigner.document.gender }}</span>
+                                        </td>
+                                        <td class="border-t border-gray-200 border-dashed phoneNumber">
+                                            <span class="px-4 text-gray-700">{{ foreigner.document.citizenship }}</span>
+                                        </td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-
-            <div>
-                <h2>Commerçants récement ajoutés</h2>
-                <NavLink class="block mb-4 no-underline" :href="route('foreigners.index', { role: 'Commerçant' })">Voir plus de Commerçants</NavLink>
-                <div class="relative overflow-x-auto overflow-y-auto bg-white rounded-lg shadow">
-                    <table class="relative w-full m-0 whitespace-no-wrap bg-white border-collapse table-auto table-striped">
-                        <thead>
-                            <tr class="text-left">
-                                <template v-for="heading in foreignerHeadings">
-                                    <th class="sticky top-0 px-6 py-2 text-xs font-bold tracking-wider text-gray-600 uppercase bg-gray-100 border-b border-gray-200"
-                                        v-text="heading.value" x-ref="heading.key" :class="{ [heading.key]: true }"></th>
-                                </template>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <template v-for="foreigner in latestTraders">
-                                <tr>
-                                    <td class="border-t border-gray-200 border-dashed userId">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.id }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed first_name">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.first_name }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed lastName">
-                                        <span class="px-4 text-gray-700 ">{{ foreigner.last_name }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed emailAddress">
-                                        <span class="px-4 text-gray-700">{{ foreigner.email }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed gender">
-                                        <span class="px-4 text-gray-700">{{ foreigner.document.gender }}</span>
-                                    </td>
-                                    <td class="border-t border-gray-200 border-dashed phoneNumber">
-                                        <span class="px-4 text-gray-700">{{ foreigner.document.citizenship }}</span>
-                                    </td>
-                                </tr>
-                            </template>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
 
         </div>
 
