6d4fef07144915b9c2dee1c5e38027188a9dae77
agahebeye <agahebeye@gmail.com>
2022-08-27 09:38:20 +0200

    create direct to Dashboard when admin logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\Hotel;
 use App\Models\Market;
 use App\Models\University;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 use Spatie\RouteAttributes\Attributes\Get;
 use Spatie\RouteAttributes\Attributes\Middleware;
 
@@ -21,8 +22,12 @@ class HomeController
      * @return \Inertia\Response
      */
     #[Get(uri: '/', name: 'home')]
-    public function __invoke(Request $request): \Inertia\Response
+    public function __invoke(Request $request): \Inertia\Response|RedirectResponse
     {
+        if ($request->user()->role->name === 'Administrateur') {
+            return redirect('/dashboard');
+        }
+        
         return inertia()->render('Home', [
             'universities' => fn () => University::select(['id', 'name'])->with('photo')->get(),
             'hotels' => fn () => Hotel::select(['id', 'name'])->with(['photo'])->get(),
