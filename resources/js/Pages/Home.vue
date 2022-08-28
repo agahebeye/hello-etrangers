3b08b1a74dc47f1f53d64da2fdfc2c7891634d8f
agahebeye <agahebeye@gmail.com>
2022-08-28 06:51:15 +0200

    wip

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -32,6 +32,16 @@ defineProps({
 
 
         <div class="pt-8 prose max-w-none">
+            <div id="hero" class="container flex flex-col items-center px-4 py-16 mx-auto text-center space-y-14 md:py-8 md:px-10 lg:px-24 xl:max-w-3xl">
+                <h1 class="m-0 text-4xl font-bold leading-none sm:text-7xl">Quisquam necessita vel
+                    <span class="text-purple-600">laborum doloribus</span>delectus
+                </h1>
+                <p class="px-8 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+                <div class="flex flex-wrap justify-center">
+                    <button class="px-8 py-3 m-2 text-lg font-semibold bg-purple-600 rounded text-gray-50">Explorer</button>
+                    <button class="px-8 py-3 m-2 text-lg text-gray-900 border border-gray-300 rounded">commander</button>
+                </div>
+            </div>
 
             <!--universities-->
             <div v-if="!isTrader" id="universities">
@@ -46,7 +56,7 @@ defineProps({
 
                 <ImageGuallery :data="markets" />
             </div>
-            
+
             <!--hotels-->
             <div v-if="!isStudent" id="hotels">
                 <h2 class="my-5 text-4xl text-gray-800">Hotels</h2>
